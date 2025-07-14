669af61b4d9c7a7290807569414c2fd335711561
Šárka Flídrová <sarka.flidrova@email.cz>
2025-07-14 13:27:26 +0200

    add basic custom directive with a console log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,9 @@ import { AuthService } from "./auth/auth.service";
   standalone: true,
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
-  imports: [AuthComponent, LearningResourcesComponent, NgIf],
+  imports: [AuthComponent, LearningResourcesComponent],
 })
 export class AppComponent {
-  private authService = inject(AuthService);
-
-  isAdmin = computed(() => this.authService.activePermission() === "admin");
+  // private authService = inject(AuthService);
+  // isAdmin = computed(() => this.authService.activePermission() === "admin");
 }
