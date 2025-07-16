174d93b5c413a7fb4fe7d5f4a34633d23f4703ee
Šárka Flídrová <sarka.flidrova@email.cz>
2025-07-16 10:02:15 +0200

    inputs in directives

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from "@angular/core";
+import { Directive, input } from "@angular/core";
 
 @Directive({
   selector: "a[appSafeLink]",
@@ -8,9 +8,14 @@ import { Directive } from "@angular/core";
   },
 })
 export class SafeLinkDirective {
+  queryParam = input("myapp", { alias: "appSafeLink" });
+
   onConfirmLeavePage(event: MouseEvent) {
     const wantsToLeave = window.confirm("Do you want to leave the app?");
     if (wantsToLeave) {
+      const address = (event.target as HTMLAnchorElement).href;
+      (event.target as HTMLAnchorElement).href =
+        address + "?from=" + this.queryParam();
       return;
     }
     event?.preventDefault();
